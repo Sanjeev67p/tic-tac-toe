55b66cce17d6dbc7b0023a289b53f7786f890fcf
Sanjeev67p <sanjeevpatwal67@gmail.com>
2023-03-03 15:19:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App(props) {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>{props.title}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  <div>
+  <h1>Hello, World</h1>          
+  </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App title = "React + Vite"/>
+    <App />
   </React.StrictMode>,
 )
